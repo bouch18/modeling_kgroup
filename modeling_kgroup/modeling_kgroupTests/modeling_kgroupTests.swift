aa058b4a5964d2912c4c70c29b969ef478936ae2
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2017-11-09 12:16:39 +0900

    project作成

---
+++ modeling_kgroup/modeling_kgroupTests/modeling_kgroupTests.swift
@@ -0,0 +1,36 @@
+//
+//  modeling_kgroupTests.swift
+//  modeling_kgroupTests
+//
+//  Created by 大城 慶知 on 2017/11/09.
+//  Copyright © 2017 Univ of the Ryukyu. All rights reserved.
+//
+
+import XCTest
+@testable import modeling_kgroup
+
+class modeling_kgroupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
